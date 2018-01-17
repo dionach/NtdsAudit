bede37125bf1a1e266ebfe51287be4bdd9eeffa9
Phill Campbell <phill.campbell@dionach.com>
2018-01-17 10:48:56 +0000

    Bump version

--- src/NtdsAudit/Properties/AssemblyInfo.cs
+++ src/NtdsAudit/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Dionach Ltd")]
 [assembly: AssemblyProduct("NtdsAudit")]
-[assembly: AssemblyCopyright("Copyright © 2017 Dionach Ltd")]
+[assembly: AssemblyCopyright("Copyright © 2018 Dionach Ltd")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,6 +32,6 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("2.0.2.0")]
-[assembly: AssemblyFileVersion("2.0.2.0")]
+[assembly: AssemblyVersion("2.0.3.0")]
+[assembly: AssemblyFileVersion("2.0.3.0")]
 [assembly: CLSCompliant(true)]
